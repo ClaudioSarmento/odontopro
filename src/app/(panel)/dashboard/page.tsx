d1e2d08da0479933fee0a7221996e8a6c82e9ea0
ClaudioSarmento <claudio-s-i@hotmail.com>
2025-06-16 11:01:49 -0300

    feature - Sidebar mobile

--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -2,7 +2,9 @@ export default function Dashboard(){
     return (
         <div>
             <h1>Página Dashboard</h1>
-        
+            <div className="w-full h-[600px] bg-gray-200 mb-10"></div>
+            <div className="w-full h-[600px] bg-gray-200 mb-10"></div>
+            <div className="w-full h-[600px] bg-gray-200 mb-10"></div>
         </div>
     )
 }
