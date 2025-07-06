15318398134bad1327b3bf2a4035f12aa67973d2
ClaudioSarmento <claudio-s-i@hotmail.com>
2025-07-06 17:25:06 -0300

    feature - criando a página serviços

---
+++ src/app/(panel)/dashboard/services/_components/service-content.tsx
@@ -0,0 +1,16 @@
+import { getAllServices } from "../_data-access/get-all-services";
+
+interface ServicesContentProps{
+    userId: string;
+}
+
+export async function ServicesContent({userId}: ServicesContentProps){
+
+    const services = await getAllServices({userId: userId});
+
+    return(
+        <div>
+            TODOS OS MEUS SERVIÇOS
+        </div>
+    )
+}
